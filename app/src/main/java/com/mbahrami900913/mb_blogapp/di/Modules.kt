cb0c5dc310f3c1bf6bfcda263bce08d1e016097c
MBahramiGitMit <mbahrami900913@gmail.com>
2023-09-17 10:05:14 +0330

    add di and Koin

---
+++ app/src/main/java/com/mbahrami900913/mb_blogapp/di/Modules.kt
@@ -0,0 +1,7 @@
+package com.mbahrami900913.mb_blogapp.di
+
+import org.koin.dsl.module
+
+val myModules= module {
+
+}
--- app/src/main/java/com/mbahrami900913/mb_blogapp/ui/MainActivity.kt
+++ app/src/main/java/com/mbahrami900913/mb_blogapp/ui/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.mbahrami900913.mb_blogapp.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
+import com.mbahrami900913.mb_blogapp.di.myModules
 import com.mbahrami900913.mb_blogapp.ui.theme.MB_BlogAppTheme
+import dev.burnoo.cokoin.Koin
+import org.koin.android.ext.koin.androidContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL
         setContent {
             MB_BlogAppTheme {
+                CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
 
+                    Koin(appDeclaration = {
+                        androidContext(this@MainActivity)
+                        modules(myModules)
+                    }) {
+
+                    }
+
+                }
             }
         }
     }
