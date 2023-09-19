9fb70b7b10daa4e2f8a07156af922677bb2fad69
MBahramiGitMit <mbahrami900913@gmail.com>
2023-09-19 09:04:17 +0330

    feat: add blog content

--- app/src/main/java/com/mbahrami900913/mb_blogapp/ui/features/BlogScreen.kt
+++ app/src/main/java/com/mbahrami900913/mb_blogapp/ui/features/BlogScreen.kt
@@ -1,11 +1,17 @@
 package com.mbahrami900913.mb_blogapp.ui.features
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.mbahrami900913.mb_blogapp.ui.widgets.BlogContent
 import com.mbahrami900913.mb_blogapp.ui.widgets.BlogToolbar
 import com.mbahrami900913.mb_blogapp.util.Cache
 import com.mbahrami900913.mb_blogapp.util.Constants
@@ -22,7 +28,18 @@ fun BlogScreen() {
             onBackClicked = { navController.navigateUp() },
             onInfoClicked = { showInfoDialog = true })
     }) {
-        it
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = it.calculateTopPadding()), contentAlignment = Alignment.BottomCenter
+        ) {
+            BlogContent(blog = blog, onImageClicked = {
+
+            })
+            if (showInfoDialog) {
+
+            }
+        }
 
     }
 }
